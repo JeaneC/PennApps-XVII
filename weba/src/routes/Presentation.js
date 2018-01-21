0ff29e3a1240645f88a864be95aca771d3183d9e
Jeane Carlos <volbohel@gmail.com>
2018-01-21 00:43:10 -0500

    af

--- weba/src/routes/Presentation.js
+++ weba/src/routes/Presentation.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import webABG from '../assets/webABG.png';
@@ -7,14 +7,14 @@ import presentation from '../assets/presentation.png';
 import edit from '../assets/edit.png';
 import folder from '../assets/folder.png';
 import logout from '../assets/logout.png';
-import {firebase, database, admin} from '../firebase/firebase';
+import { firebase, database, admin } from '../firebase/firebase';
 import Iframe from 'react-iframe';
 import Dashboard from '../components/Dashboard';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const pptId =
-    'https://docs.google.com/presentation/d/e/2PACX-1vQMWeVYHk5NjwNqLjM-wcxqQK8qcVhdi53wprdAIl7Mqy7Xx1je9JdaaOn7RUMHK0jrejPLPqJDxibX/embed?start=false&loop=false&delayms=3000';
+	'https://docs.google.com/presentation/d/e/2PACX-1vQMWeVYHk5NjwNqLjM-wcxqQK8qcVhdi53wprdAIl7Mqy7Xx1je9JdaaOn7RUMHK0jrejPLPqJDxibX/embed?start=false&loop=false&delayms=3000';
 const Container = styled.div`
 	display: flex;
 	flex: 1;
@@ -91,7 +91,6 @@ const Transcript = styled.div`
 const BubbleContainer = styled.div`
 	width: 90%;
 	min-height: 110px;
-	max-height: 110px;
 	display: flex;
 	flex-direction: column;
 	align-items: flex-start;
@@ -124,122 +123,120 @@ const BubbleText = styled.p`
 `;
 //#f8eee7
 class Presentation extends Component {
-    state = {
-        chatList: []
-    };
+	state = {
+		chatList: []
+	};
 
-    componentDidMount() {
-    }
+	componentDidMount() {}
 
-    render() {
-        return (
-            <Container>
-                <Dashboard />
-                <Body>
-                <Frame
-                    src={pptId}
-                    width="100%"
-                    height="100%"
-                    frameBorder="0"
-                    allowtransparency="true"
-                    style={{backgroundColor: 'white'}}
-                />
-                <Transcript>
-                    {this.state.chatList.map(bubble => {
-                        return (
-                            <BubbleContainer>
-                                <TextCaption>
-                                    {bubble.timeStamp} - Slide {bubble.slide}
-                                </TextCaption>
-                                <Bubble>
-                                    <BubbleText>{bubble.text}</BubbleText>
-                                </Bubble>
-                            </BubbleContainer>
-                        );
-                    })}
-                </Transcript>
-                </Body>
-            </Container>
-        );
-    }
+	render() {
+		return (
+			<Container>
+				<Dashboard />
+				<Body>
+					<Frame
+						src={pptId}
+						width="100%"
+						height="100%"
+						frameBorder="0"
+						allowtransparency="true"
+						style={{ backgroundColor: 'white' }}
+					/>
+					<Transcript>
+						{this.state.chatList.map(bubble => {
+							return (
+								<BubbleContainer>
+									<TextCaption>
+										{bubble.timeStamp} - Slide {bubble.slide}
+									</TextCaption>
+									<Bubble>
+										<BubbleText>{bubble.text}</BubbleText>
+									</Bubble>
+								</BubbleContainer>
+							);
+						})}
+					</Transcript>
+				</Body>
+			</Container>
+		);
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            chatList: [],
-            firstItem: true
-        };
-        this.addBubble = this.addBubble.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			chatList: [],
+			firstItem: true
+		};
+		this.addBubble = this.addBubble.bind(this);
+	}
 
-    addBubble(bubbleObj) {
-        const bubbleObjects = [...this.state.chatList, bubbleObj];
-        this.setState({chatList: bubbleObjects});
-    }
+	addBubble(bubbleObj) {
+		const bubbleObjects = [...this.state.chatList, bubbleObj];
+		this.setState({ chatList: bubbleObjects });
+	}
 
-    componentDidMount() {
-        firebase
-            .auth()
-            .signInAnonymously()
-            .catch(function (error) {
-                let errorCode = error.code;
-                let errorMessage = error.message;
-                console.log('Error ' + errorCode + ': ' + errorMessage);
-            });
-        let transcriptRef = database.ref('Classes/67445/Transcript');
-        transcriptRef.on('value', snapshot => {
-            console.log(this.state.firstItem);
-            if (this.state.firstItem === false) {
-                let lastAdded = snapshot.val()[
-                    Object.keys(snapshot.val())[Object.keys(snapshot.val()).length - 1]
-                    ];
-                this.addBubble({
-                    timeStamp: lastAdded.Timestamp,
-                    slide: lastAdded.Slide,
-                    text: lastAdded.Text
-                });
-            }
-            this.state.firstItem ? this.setState({firstItem: false}) : 1 + 1;
-        });
-    }
+	componentDidMount() {
+		firebase
+			.auth()
+			.signInAnonymously()
+			.catch(function(error) {
+				let errorCode = error.code;
+				let errorMessage = error.message;
+				console.log('Error ' + errorCode + ': ' + errorMessage);
+			});
+		let transcriptRef = database.ref('Classes/67445/Transcript');
+		transcriptRef.on('value', snapshot => {
+			console.log(this.state.firstItem);
+			if (this.state.firstItem === false) {
+				let lastAdded = snapshot.val()[
+					Object.keys(snapshot.val())[Object.keys(snapshot.val()).length - 1]
+				];
+				this.addBubble({
+					timeStamp: lastAdded.Timestamp,
+					slide: lastAdded.Slide,
+					text: lastAdded.Text
+				});
+			}
+			this.state.firstItem ? this.setState({ firstItem: false }) : 1 + 1;
+		});
+	}
 
-
-    render() {
-        return (
-            <Container>
-                <Dashboard />
-                <Body>
-                <Frame
-                    src={pptId}
-                    width="50%"
-                    height="100%"
-                    frameBorder="0"
-                    allowtransparency="true"
-                    style={{backgroundColor: 'white'}}
-                />
-                <Transcript>
-                    {this.state.chatList.map((bubble, i) => {
-                        return (
-                            <BubbleContainer key={i}>
-                                <TextCaption>
-                                    {bubble.timeStamp} - Slide {bubble.slide}
-                                </TextCaption>
-                                <Bubble>
-                                    <BubbleText>{bubble.text}</BubbleText>
-                                </Bubble>
-                            </BubbleContainer>
-                        );
-                    })}
-                </Transcript>
-                </Body>
-            </Container>
-        );
-    }
+	render() {
+		return (
+			<Container>
+				<Dashboard />
+				<Body>
+					<Frame
+						src={pptId}
+						width="50%"
+						height="100%"
+						frameBorder="0"
+						allowtransparency="true"
+						style={{ backgroundColor: 'white' }}
+					/>
+					<Transcript>
+						{this.state.chatList.map((bubble, i) => {
+							return (
+								<BubbleContainer key={i}>
+									<TextCaption>
+										{bubble.timeStamp} - Slide {bubble.slide}
+									</TextCaption>
+									<Bubble>
+										<BubbleText>{bubble.text}</BubbleText>
+									</Bubble>
+								</BubbleContainer>
+							);
+						})}
+					</Transcript>
+				</Body>
+			</Container>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-    console.log('Presentation', state);
-    return {};
+	console.log('Presentation', state);
+	return {};
 };
 
 export default connect(mapStateToProps)(Presentation);
