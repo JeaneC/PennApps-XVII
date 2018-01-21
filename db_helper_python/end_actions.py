e70237c2e6c86dd4a501039d208b3f588aeac04a
Ali Afzal <ali.afzal@stonybrook.edu>
2018-01-21 04:55:12 -0500

    Added Sentiment Detection
    Added sentiment detection and emotion detection to the transcript.
    Also parsed key words from the lecture transcript and generated wikipedia links for further study (resources section)

--- db_helper_python/end_actions.py
+++ db_helper_python/end_actions.py
@@ -3,7 +3,9 @@
 import pyrebase
 from gensim.summarization import summarize
 import time
-import traceback
+from paralleldots import set_api_key, get_api_key 
+from paralleldots import similarity, ner, taxonomy, sentiment, keywords, intent, emotion, multilang, abuse
+
 config={
 	"apiKey": 'AIzaSyAf2GzYCTV-ocPtJ6Pk3Hvk_4sObLjYKic',
 	"authDomain": 'classroombeta-87cdc.firebaseapp.com',
@@ -24,6 +26,7 @@ user = auth.sign_in_with_email_and_password('jeane.carlos15@stjohns.edu', 'passw
 db = firebase.database()
 storage = firebase.storage()
 
+set_api_key("BcpOcRiBWENPmQkyaerFUR5P8otXKfhAwQvXdID4dio")
 
 def stream_handler(message):
 	global counter, didEnd
@@ -56,15 +59,46 @@ try:
 				sentenceCount += 1
 				
 			toPut = summarize(completeParagraph, ratio = 0.4)
-			print(toPut)
 			
+			pSentiment = sentiment(completeParagraph)
+			pEmotion = emotion(completeParagraph)
+			pKeywords = keywords(completeParagraph)
+			
+			links = []
+			for thing in pKeywords['keywords']:
+				if thing['confidence_score'] > 0.86:
+					db.child("Classes/67445/Links").push(
+							{"Link": "https://en.wikipedia.org/wiki/" + thing['keyword']}, user['idToken'])
+			
+			
+			pSentimentProbabilities = pSentiment['probabilities']
+			pEmotionProbabilities = pEmotion['probabilities']
+			
+			#upload sentiment info
+			db.child("Classes/67445/Sentiment").update(
+						{"Negative": pSentimentProbabilities['positive'],
+						 "Positive": pSentimentProbabilities['negative'],
+						 "Neutral": pSentimentProbabilities['neutral'],
+						 "Overall": pSentiment['sentiment']}, user['idToken'])
+						 
+			db.child("Classes/67445/Emotion").update(
+						{"Angry": pEmotionProbabilities['angry'],
+						 "Excited": pEmotionProbabilities['excited'],
+						 "Happy": pEmotionProbabilities['happy'],
+						 "Indifferent": pEmotionProbabilities['indifferent'],
+						 "Overall": pEmotion['emotion'],
+						 "Sad": pEmotionProbabilities['sad'],}, user['idToken'])
+			
+			
+			
+						 
 			db.child("Classes/67445").update({"Summary": toPut}, user['idToken'])
 			db.child("Classes/67445").update({"End": False}, user['idToken'])
 			
-			time.sleep(5)
 except KeyboardInterrupt:
 	my_stream.close()
 except Exception:
 	my_stream.close()
+	
 	traceback.printexc()
 	
